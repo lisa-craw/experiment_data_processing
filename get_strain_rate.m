63cc5d606b06b796e0787422aef6124a55bef181
Lisa Craw <lisa.craw@utas.edu.au>
2022-03-11 14:49:52 +1100

    added in better functionality to strain rate selection process

--- get_strain_rate.m
+++ get_strain_rate.m
@@ -3,21 +3,36 @@
 
 function [secondary_rate, secondary_strain, tertiary_rate, tertiary_strain] = get_strain_rate(experiment_number)
 	filepath = strcat('~/experimental_data/', experiment_number, '/', experiment_number, '_smoothed.csv');
-
 		table = readtable(filepath);
 		rate = table.smoothed_octahedral_strain_rate;
 		strain = table.smoothed_octahedral_strain;
-		plot(rate)
+		semilogy(rate)
+		axis([0 length(rate)*1.5 min(rate)*0.8 max(rate)*1.7])
+		hold on
+		title(experiment_number)
+		text((length(rate)/2), max(rate)*1.2, ['no secondary minimum?' char(10) 'click here'], 'HorizontalAlignment', 'center'); 
+		text((length(rate)*1.3), (max(rate)-min(rate))/1.5, ['no steady tertiary rate?' char(10) 'click here'], 'HorizontalAlignment', 'center'); 
 		disp('select the secondary minimum')
 		[sec_x, sec_y] = ginput(1);
-		secondary_rate = sec_y;
-		secondary_strain = strain(floor(sec_x));
-	
+		if sec_y<=max(rate)
+			secondary_rate = sec_y;
+			secondary_strain = strain(floor(sec_x));
+		else 
+			secondary_rate = NaN;
+			secondary_strain = NaN;
+		end
+				
 		disp('select the start and end of the tertiary rate')
 		%this is to exclude the "drop-off zone" at the end of some experiments
 		[tert_x, tert_y] = ginput(2);
 		tert_x = floor(tert_x);
 		tert_y = floor(tert_y);
-		tertiary_rate = mean(rate(tert_x(1):tert_x(2)));
-		tertiary_strain = mean(strain(tert_x(1):tert_x(2)));
+		clf	
+		if tert_x<=length(rate)
+			tertiary_rate = mean(rate(tert_x(1):tert_x(2)));
+			tertiary_strain = mean(strain(tert_x(1):tert_x(2)));
+		else
+			tertiary_rate = NaN;
+			tertiary_strain = NaN;
+		end
 end
--- strain_rates_tabulate.m
+++ strain_rates_tabulate.m
@@ -1,20 +1,27 @@
 % function to iterate through a list of experiments, and add secondary and tertiary strain rates to a table based on user-selected points
 
-experiment_list = readtable('~/experimental_data/strain_rates.csv');
+%if this is the first time, we take the data from the experiment parameters table. If we've run this before, we will load the output tanle from last time, so that we can add data for any experiment at any time 
+if isfile('~/experimental_data/strain_rates.csv')
+	experiment_list = readtable('~/experimental_data/strain_rates.csv');
+else
+	experiment_list = readtable('~/experimental_data/experiment_parameters.csv');
+end
 
 %list of experiments we wish to calculate strain rates for (so we don't have to do them all every time)
-target_experiments = [1 2 3 4]' ;
+target_experiments = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43]';
+%target_experiments = [4]'; 
 
 for i=1:length(target_experiments)
-	experiment_number = char(experiment_list.experiment_number(i));
-	if isfile(strcat('~/experimental_data/', experiment_number, '/', experiment_number, '_smoothed.csv'))
-		[secondary_rate, secondary_strain, tertiary_rate, tertiary_strain] = get_strain_rate(experiment_number)
-		experiment_list.secondary_rate(i) = secondary_rate;
-		experiment_list.secondary_strain(i) = secondary_strain;
-		experiment_list.tertiary_rate(i) = tertiary_rate;
-		experiment_list.tertiary_strain(i) = tertiary_strain;
+	experiment_number = target_experiments(i);
+	experiment_name = char(experiment_list.experiment_number(experiment_number));
+	if isfile(strcat('~/experimental_data/', experiment_name, '/', experiment_name, '_smoothed.csv'))
+		[secondary_rate, secondary_strain, tertiary_rate, tertiary_strain] = get_strain_rate(experiment_name)
+		experiment_list.secondary_rate(i) = round(secondary_rate, 4, 'significant');
+		experiment_list.secondary_strain(i) = round(secondary_strain, 4, 'significant');
+		experiment_list.tertiary_rate(i) = round(tertiary_rate, 4, 'significant');
+		experiment_list.tertiary_strain(i) = round(tertiary_strain, 4, 'significant');
 	else
 	end
 end
 
-writetable(experiment_list, '~/experimental_data/strain_rates_test.csv')
+writetable(experiment_list, '~/experimental_data/strain_rates.csv')
