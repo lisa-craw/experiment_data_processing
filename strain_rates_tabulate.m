af45297ab9ade6f3eba7ac2fd2e006b0e1bda229
Lisa Craw <lisa.craw@utas.edu.au>
2022-04-05 13:53:38 +1000

    fixed bug causing strain rates to be incorrectly recorded

--- strain_rates_tabulate.m
+++ strain_rates_tabulate.m
@@ -1,6 +1,6 @@
 % function to iterate through a list of experiments, and add secondary and tertiary strain rates to a table based on user-selected points
 
-%if this is the first time, we take the data from the experiment parameters table. If we've run this before, we will load the output tanle from last time, so that we can add data for any experiment at any time 
+%if this is the first time, we take the data from the experiment parameters table. If we've run this before, we will load the output table from last time, so that we can add data for any experiment at any time 
 if isfile('~/experimental_data/strain_rates.csv')
 	experiment_list = readtable('~/experimental_data/strain_rates.csv');
 else
@@ -8,20 +8,19 @@ else
 end
 
 %list of experiments we wish to calculate strain rates for (so we don't have to do them all every time)
-target_experiments = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43]';
-%target_experiments = [4]'; 
+target_experiments = [1 2 4 5 8 9 10 11 12 13 16 17 18 19 20 22 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45]';
 
 for i=1:length(target_experiments)
 	experiment_number = target_experiments(i);
 	experiment_name = char(experiment_list.experiment_number(experiment_number));
 	if isfile(strcat('~/experimental_data/', experiment_name, '/', experiment_name, '_smoothed.csv'))
 		[secondary_rate, secondary_strain, tertiary_rate_1, tertiary_strain_1, tertiary_rate_2, tertiary_strain_2] = get_strain_rate(experiment_name)
-		experiment_list.secondary_rate(i) = round(secondary_rate, 4, 'significant');
-		experiment_list.secondary_strain(i) = round(secondary_strain, 4, 'significant');
-		experiment_list.tertiary_rate_1(i) = round(tertiary_rate_1, 4, 'significant');
-		experiment_list.tertiary_strain_1(i) = round(tertiary_strain_1, 4, 'significant');
-		experiment_list.tertiary_rate_2(i) = round(tertiary_rate_2, 4, 'significant');
-		experiment_list.tertiary_strain_2(i) = round(tertiary_strain_2, 4, 'significant');
+		experiment_list.secondary_rate(experiment_number) = round(secondary_rate, 4, 'significant');
+		experiment_list.secondary_strain(experiment_number) = round(secondary_strain, 4, 'significant');
+		experiment_list.tertiary_rate_1(experiment_number) = round(tertiary_rate_1, 4, 'significant');
+		experiment_list.tertiary_strain_1(experiment_number) = round(tertiary_strain_1, 4, 'significant');
+		experiment_list.tertiary_rate_2(experiment_number) = round(tertiary_rate_2, 4, 'significant');
+		experiment_list.tertiary_strain_2(experiment_number) = round(tertiary_strain_2, 4, 'significant');
 	else
 	end
 end
