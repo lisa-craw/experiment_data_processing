f0f65716b090be2320786f94cfb22ac15bf691a9
Lisa Craw <lisa.craw@utas.edu.au>
2022-04-06 23:27:43 +1000

    fix bugs with colours and legends

--- plotting_scripts/plot_rheology_stress_strain.m
+++ plotting_scripts/plot_rheology_stress_strain.m
@@ -1,37 +1,40 @@
 % script to plot stress vs. strain data from all rheology experiments (excludes temperature change and microstructure evolution experiments, i.e. only constant temp experiments which reached teriary strain)
 
-%% select only experiments which reached tertiary and didn't have a temperature change. Also remove -10 and -5 experiments, as they aren't relevant here {{{
+%% select only experiments which reached tertiary and didn't have a temperature change. {{{
 strain_rates = readtable('~/experimental_data/strain_rates.csv');
 
 changing_temp = contains(strain_rates.temperature, ',');
 rheology_list = strain_rates(~changing_temp, :);
 not_tertiary = isnan(rheology_list.tertiary_rate_1);
 rheology_list = rheology_list(~not_tertiary, :);
-minus_10 = str2double(rheology_list.temperature)==-10;
-rheology_list = rheology_list(~minus_10, :);
 %%}}}
 
 % separate out marine, meteoric and standard ice {{{ 
 % sort by temperature to make legend labels easier
 is_standard = strcmp(rheology_list.ice_type, 'standard');
 standard_ice = rheology_list(is_standard, :);
-standard_ice = sortrows(standard_ice, 'temperature');
+standard_ice.temperature = str2double(standard_ice.temperature);
+standard_ice = sortrows(standard_ice, 'temperature', 'descend');
 is_marine = strcmp(rheology_list.ice_type, 'marine');
 marine_ice = rheology_list(is_marine, :);
-marine_ice = sortrows(marine_ice, 'temperature');
+marine_ice.temperature = str2double(marine_ice.temperature);
+marine_ice = sortrows(marine_ice, 'temperature', 'descend');
 is_meteoric = strcmp(rheology_list.ice_type, 'meteoric');
 meteoric_ice = rheology_list(is_meteoric, :);
-meteoric_ice = sortrows(meteoric_ice, 'temperature');
+meteoric_ice.temperature = str2double(meteoric_ice.temperature);
+meteoric_ice = sortrows(meteoric_ice, 'temperature', 'descend');
 
 %}}}
 
 %% set colours {{{
 
+shade = 0.15; % amount by which to adjust colours for different temperatures
+
 %purple for standard ice
-shade = 0.15;
-c_standard_5 = [0.4431 0.2431 0.3529] + [shade shade shade];
-c_standard_2 = c_standard_5 + [shade shade shade];
-c_standard_7 = c_standard_5 - [shade shade shade];
+c_standard_7 = [0.4431 0.2431 0.3529];
+c_standard_10 = c_standard_7 - 0.5*[shade shade shade];
+c_standard_5 = c_standard_7 + [shade shade shade];
+c_standard_2 = c_standard_7 + 2*[shade shade shade];
 
 %blue for meteoric ice
 c_meteoric_7 = [0.2157 0.4431 0.5569];
@@ -60,31 +63,31 @@ standard_ax = subplot(1,3,1);
 hold on
 		
 % find the first instance of each temp (these need to be visible to the legend)
-minus2_ind = find(str2double(standard_ice.temperature)==-2);
+minus2_ind = find(standard_ice.temperature==-2);
 first_minus2 = minus2_ind(1);
-minus7_ind = find(str2double(standard_ice.temperature)==-7);
+minus7_ind = find(standard_ice.temperature==-7);
 first_minus7 = minus7_ind(1);
-minus5_ind = find(str2double(standard_ice.temperature)==-5);
+minus5_ind = find(standard_ice.temperature==-5);
 first_minus5 = minus5_ind(1);
-%minus10_ind = find(str2double(standard_ice.temperature)==-10);
-%first_minus10 = minus10_ind(1);
+minus10_ind = find(standard_ice.temperature==-10);
+first_minus10 = minus10_ind(1);
 
 for i = 1:length(standard_ice.experiment_number)
 	experiment_number = char(standard_ice.experiment_number(i));
 	filepath = strcat('~/experimental_data/', experiment_number, '/', experiment_number, '_smoothed.csv');
 	if isfile(filepath)
 		data = readtable(filepath);
-		if str2double(standard_ice.temperature{i}) == -7;
+		if standard_ice.temperature(i) == -7;
 			colour = c_standard_7;
-		elseif str2double(standard_ice.temperature{i}) == -2;
+		elseif standard_ice.temperature(i) == -2;
 			colour = c_standard_2;
-		elseif str2double(standard_ice.temperature{i}) == -5;
+		elseif standard_ice.temperature(i) == -5;
 			colour = c_standard_5;
-%		elseif str2double(standard_ice.temperature{i}) == -10;
-%			colour = c_standard_10;
+		elseif standard_ice.temperature(i) == -10;
+			colour = c_standard_10;
 		else colour = [0.5 0.5 0.5];
 		end
-		if i==first_minus7 | i==first_minus2 | i==first_minus5% | i==first_minus10
+		if i==first_minus7 | i==first_minus2 | i==first_minus5 | i==first_minus10
 			handle_visibility = 'on';
 		else
 			handle_visibility = 'off';
@@ -97,7 +100,7 @@ for i = 1:length(standard_ice.experiment_number)
 	end
 end
 axis(axis_limits)
-legend('$-2\,^{\circ}\mathrm{C}$', '$-5\,^{\circ}\mathrm{C}$', '$-7\,^{\circ}\mathrm{C}$')
+legend('$-2\,^{\circ}\mathrm{C}$', '$-5\,^{\circ}\mathrm{C}$', '$-7\,^{\circ}\mathrm{C}$', '$-10\,^{\circ}\mathrm{C}$')
 xlabel('octahedral shear strain')
 ylabel('octahedral shear strain rate')
 title('standard ice')
@@ -108,7 +111,7 @@ meteoric_ax = subplot(1,3,2);
 hold on
 		
 % find the first instance of each temp (these need to be visible to the legend)
-minus7_ind = find(str2double(meteoric_ice.temperature)==-7);
+minus7_ind = find(meteoric_ice.temperature==-7);
 first_minus7 = minus7_ind(1);
 
 for i = 1:length(meteoric_ice.experiment_number)
@@ -116,9 +119,9 @@ for i = 1:length(meteoric_ice.experiment_number)
 	filepath = strcat('~/experimental_data/', experiment_number, '/', experiment_number, '_smoothed.csv');
 	if isfile(filepath)
 		data = readtable(filepath);
-		if str2double(meteoric_ice.temperature{i}) == -7;
+		if meteoric_ice.temperature(i) == -7;
 			colour = c_meteoric_7;
-		elseif str2double(meteoric_ice.temperature{i}) == -2;
+		elseif meteoric_ice.temperature(i) == -2;
 			colour = c_meteoric_2;
 		else colour = [0.5 0.5 0.5];
 		end
@@ -146,9 +149,9 @@ marine_ax = subplot(1,3,3);
 hold on
 		
 % find the first instance of each temp (these need to be visible to the legend)
-minus2_ind = find(str2double(marine_ice.temperature)==-2);
+minus2_ind = find(marine_ice.temperature==-2);
 first_minus2 = minus2_ind(1);
-minus7_ind = find(str2double(marine_ice.temperature)==-7);
+minus7_ind = find(marine_ice.temperature==-7);
 first_minus7 = minus7_ind(1);
 
 for i = 1:length(marine_ice.experiment_number)
@@ -156,9 +159,9 @@ for i = 1:length(marine_ice.experiment_number)
 	filepath = strcat('~/experimental_data/', experiment_number, '/', experiment_number, '_smoothed.csv');
 	if isfile(filepath)
 		data = readtable(filepath);
-		if str2double(marine_ice.temperature{i}) == -7;
+		if marine_ice.temperature(i) == -7;
 			colour = c_marine_7;
-		elseif str2double(marine_ice.temperature{i}) == -2;
+		elseif marine_ice.temperature(i) == -2;
 			colour = c_marine_2;
 		else colour = [0.5 0.5 0.5];
 		end
