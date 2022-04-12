7b29c597f523cc06b38f133868081075e2fb4622
Lisa Craw <lisa.craw@utas.edu.au>
2022-04-12 16:09:37 +1000

    update load corrections

--- correction_scripts/LC044_corrections.m
+++ correction_scripts/LC044_corrections.m
@@ -6,11 +6,11 @@ data.h_dis = tmp.v_dis;
 data.time = tmp.time;
 data.temp = tmp.temp;
 
-%data.v_dis(95725:end) = data.v_dis(95725:end) - 0.012; 
+data.v_dis(60262:end) = data.v_dis(60262:end) - 0.003; 
 
-%I = [95597 95725];
-%p = polyfit(I,[data.v_dis(I(1)) data.v_dis(I(2))],1);
-%data.v_dis(I(1):I(2)) = polyval(p,I(1):I(2)); 
+I = [59570 60262];
+p = polyfit(I,[data.v_dis(I(1)) data.v_dis(I(2))],1);
+data.v_dis(I(1):I(2)) = polyval(p,I(1):I(2)); 
 
 %data_corr = data; 
 writetable(struct2table(data), output_file); 
