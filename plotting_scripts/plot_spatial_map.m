a01fe68ec9e827cfa28d5ccac5d973a6f4f6b70e
Lisa Craw <lisa.craw@utas.edu.au>
2022-04-21 15:53:10 +1000

    change line width

--- plotting_scripts/plot_spatial_map.m
+++ plotting_scripts/plot_spatial_map.m
@@ -3,20 +3,34 @@
 
 function plot_spatial_map(input_path, output_path)
 	load(input_path)
-	fig = newMtexFigure;	
+	fig1 = newMtexFigure;	
 %	plot(ebsd('ice'),color, 'micronbar', 'off');
 	plot(ebsd('Ice 1h'),trendColor,'micronbar','on')
 
 	hold on
 	bnd = grains.boundary('ice','ice');
-	plot(grains.boundary,'linewidth',0.5, 'LineColor', 'k')
+	plot(grains.boundary,'linewidth',1, 'LineColor', 'k')
 
-	mp = getappdata(fig.children(1),'mapPlot');
+	mp = getappdata(fig1.children(1),'mapPlot');
 	mp.micronBar.length=5;
 
 %	export_fig(output_path, '-png')
+	filename = strcat(output_path, '_scale.png');
+	saveFigure(filename); 
+
+	close all
+	
+	fig2 = newMtexFigure;	
+%	plot(ebsd('ice'),color, 'micronbar', 'off');
+	plot(ebsd('Ice 1h'),trendColor,'micronbar','off')
+
+	hold on
+	bnd = grains.boundary('ice','ice');
+	plot(grains.boundary,'linewidth',1, 'LineColor', 'k')
+
 	filename = strcat(output_path, '.png');
 	saveFigure(filename); 
+
 	close all
 end
 
