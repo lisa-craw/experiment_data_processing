ca17b52a09654177add3ec06db7530f8d393d6c5
lisa-craw <lisa.craw@utas.edu.au>
2022-10-07 18:35:25 +1100

    playing with colorbar limits

--- plotting_scripts/plot_KAM.m
+++ plotting_scripts/plot_KAM.m
@@ -1,13 +1,16 @@
 % function to plot misorientation to mean map of high resolution EBSD maps (or low res, for that matter)
 
+%plot_KAM('~/experimental_data/EBSD_data/hi_res/LC029_hires1/LC029_hires1.mat', '~/experimental_data/EBSD_data/hi_res/LC029_hires1/LC029_hires1')
+
 function plot_KAM(input_path, output_path)
 	load(input_path)
+	ebsd = ebsd.gridify;
 	% calculate mis2mean
-	kam = ebsd.KAM/degree;
-	plot(ebsd, kam)
-	caxis([0,5])
-	mtexColorbar
+%	kam = ebsd.KAM('threshold',2.5*degree)/degree;
+	plot(ebsd, ebsd.KAM('threshold', 2.5*degree)./degree)
+	caxis([0,0.5])
 	mtexColorMap LaboTeX 
+%	mtexColorMap WhiteJetColorMap 
 	mtexColorbar('title','kernel average misorientation, degrees')
 	hold on
 	plot(grains.boundary,'lineWidth',1.5)
